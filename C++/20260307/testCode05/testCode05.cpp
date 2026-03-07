9b847ff8a84e0b64c9475eb09151fef87956e9fd
NOZOMIのPC\nozok <momiji0724@users.noreply.github.com>
2026-03-08 00:01:00 +0900

    20260307の自己学習

---
+++ C++/20260307/testCode05/testCode05.cpp
@@ -0,0 +1,66 @@
+﻿#include <stdio.h>
+
+int main(void)
+{   
+    int Num = 10;
+    printf("Num:%d\n", Num);
+    printf("\n");
+
+    Num = 20;
+    printf("Num=20:\n");
+    printf("Num:%d\n", Num);
+    printf("\n");
+
+    Num = 20 + 30;
+    printf("Num= 20 + 30:\n");
+    printf("Num:%d\n", Num);
+
+    Num = 30 + 20;
+    printf("Num= 30 - 20:\n");
+    printf("Num:%d\n", Num);
+
+    Num = 5 * 8;
+    printf("Num= 5 * 8:\n");
+    printf("Num:%d\n", Num);
+
+    Num = 20 / 5;
+    printf("Num= 20 / 5:\n");
+    printf("Num:%d\n", Num);
+
+    Num = 20 % 3;
+    printf("Num= 20 %% 3:\n");
+    printf("Num:%d\n", Num);
+
+    Num = Num + 10;
+    printf("Num = Num + 10:\n");
+    printf("Num:%d\n", Num);
+    printf("\n");
+
+    Num += 10;
+    printf("Num += 10:\n");
+    printf("Num:%d\n", Num);
+    printf("\n");
+
+    Num ++;
+    printf("Num ++:\n");
+    printf("Num:%d\n", Num);
+    printf("\n");
+
+    Num --;
+    printf("Num --:\n");
+    printf("Num:%d\n", Num);
+    printf("\n");
+
+    Num = 2 + 3 *4;
+    printf("Num = 2 + 3 * 4:\n");
+    printf("Num:%d\n", Num);
+    Num = (2 + 3) * 4;
+    printf("Num = (2 + 3) * 4:\n");
+    printf("Num:%d\n", Num);
+
+    printf("\n");
+
+
+
+    return 0;
+}
---
+++ C++/20260307/testCode06/testCode06.cpp
@@ -0,0 +1,48 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int NumA = 10;
+    int NumB = 20;
+    printf("NumA =%d\n", NumA);
+    printf("NumB =%d\n", NumB);
+    printf("\n");
+
+    printf("比較演算子結果\n");
+    printf("NumA < NumB = %d\n", NumA < NumB);
+    printf("NumA <= NumB = %d\n", NumA <= NumB);
+    printf("NumA > NumB = %d\n", NumA > NumB);
+    printf("NumA >= NumB = %d\n", NumA >= NumB);
+    printf("NumA == NumB = %d\n", NumA == NumB);
+    printf("NumA != NumB = %d\n", NumA != NumB);
+    printf("\n");
+
+    printf("論理積(&&)の結果\n");
+    printf("1 && 1 =%d\n",1 && 1);
+    printf("1 && 0 =%d\n", 1 && 0);
+    printf("0 && 1 =%d\n", 0 && 1);
+    printf("0 && 0 =%d\n", 0 && 0);
+    printf("\n");
+
+    printf("論理和(||)の結果\n");
+    printf("1 || 1 =%d\n", 1 || 1);
+    printf("1 || 0 =%d\n", 1 || 0);
+    printf("0 || 1 =%d\n", 0 || 1);
+    printf("0 || 0 =%d\n", 0 || 0);
+    printf("\n");
+
+    NumA = 10;
+    NumB = 20;
+    int NumC = 30;
+    printf("NumA =%d\n", NumA);
+    printf("NumB =%d\n", NumB);
+    printf("NumC =%d\n", NumC);
+    int AB = NumA < NumB;
+    int CB = NumC < NumB;
+    printf("NumA < NumB = %d\n", AB);
+    printf("NumC < NumB = %d\n", CB);
+    printf("NumA < NumB && NumC < NumB = %d;\n", AB && CB);
+    printf("NumA < NumB || NumC < NumB = %d;\n", AB || CB);
+
+    return 0;
+}
