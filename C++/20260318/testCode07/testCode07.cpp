929da5287131e582e0d7cce4318e03d9073ecc4f
NOZOMIのPC\nozok <momiji0724@users.noreply.github.com>
2026-03-19 00:01:19 +0900

    20260319の自己学習

---
+++ C++/20260318/testCode07/testCode07.cpp
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+    int Age;
+    printf("未成年には販売できません\n");
+    printf("貴方の年齢は？\n");
+    scanf("%d", &Age);
+
+    if(Age >= 20)
+    {
+        printf("お買い上げありがとうございます\n");
+    }
+    else
+    {
+        printf("販売できません\n");
+    }
+
+    printf("またのご来店お待ちしております。\n");
+
+    return 0;
+}
---
+++ C++/20260318/testCode08/testCode08.cpp
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+    int Age;
+    int Money = 10000;;
+
+    printf("未成年には販売できません\n");
+    printf("貴方の年齢は？\n");
+    scanf("%d", &Age);
+
+    if(Age >= 20)
+    {
+        int iCount;
+        printf("一つ980円です、いくつ買いますか？\n");
+        scanf("%d",&iCount);
+
+        Money -= 980 * iCount;
+
+        printf("お買い上げありがとうございます。\n");
+        printf("残り所持金 %d 円\n", Money);
+    }
+    else
+    {
+        printf("販売できません。\n");
+    }
+
+    return 0;
+}
+
