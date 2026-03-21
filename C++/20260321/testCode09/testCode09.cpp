526c56e2cc5bc21efe05a2f65e32607b5c55ed9a
NOZOMIのPC\nozok <momiji0724@users.noreply.github.com>
2026-03-21 23:57:11 +0900

    20260321の自己学習

---
+++ C++/20260321/testCode09/testCode09.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+    int Count = 0;
+    while(Count < 5)
+    {
+        printf("%d回目の繰り返しです\n", Count);
+
+        Count++;
+    }
+    printf("繰り返しが終了しました、%d回繰り返しました\n",Count);
+    return 0;
+}
---
+++ C++/20260321/testCode10/testCode10.cpp
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    
+    for(int Count = 0;Count < 5;Count++)
+    {
+        printf("%d回目の繰り返しです\n", Count);
+    }
+    printf("繰り返しが終了しました\n");
+    return 0;
+}
