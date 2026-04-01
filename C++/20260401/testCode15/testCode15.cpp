b3873c11cebed4cfdd4d854eafde03091cae0cef
NOZOMIのPC\nozok <momiji0724@users.noreply.github.com>
2026-04-01 23:03:33 +0900

    20260401の自己学習

---
+++ C++/20260401/testCode15/testCode15.cpp
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+    int Data;
+    printf("実行したい処理の番号を入力してください : ");
+    scanf("%d", &Data);
+
+    switch(Data)
+    {
+    case 0:
+        printf("0の場合の処理を実行\n");
+        break;
+    case 1:
+        printf("1の場合の処理を実行\n");
+        break;
+    default:
+        printf("それ以外の数値です\n");
+        break;
+    }
+    return 0;
+}
---
+++ C++/20260401/testCode16/testCode16.cpp
@@ -0,0 +1,28 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+enum SCENE
+{
+    TITLE,
+    GAME,
+    RESULT = 5,
+    CLEAR,
+};
+
+int main(void)
+{
+    int Data;
+    printf("実行したい処理の番号を入力してください : ");
+    scanf("%d", &Data);
+
+    switch (Data)
+    {
+    case TITLE:
+        printf("TITLEの場合の処理を実行\n");
+        break;
+    case GAME:
+        printf("GAMEの場合の処理を実行\n");
+        break;
+    }
+    return 0;
+}
